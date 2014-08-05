fe71418fd51554d15337f6772b689ac75e953b19
Rolf Jagerman <rjagerman@gmail.com>
2014-08-05 16:43:36 +0200

    Fixed tests and added left-pad zeroes to ensure same length hashes

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,22 +9,18 @@ close(f)
 for test in tests
 
     if "seed" in keys(test)
-        x86_32 = hex(Murmur3.x86.hash32(test["text"], test["seed"]))
-        x86_128 = hex(Murmur3.x86.hash128(test["text"], test["seed"]))
-        x64_128 = hex(Murmur3.x64.hash128(test["text"], test["seed"]))
+        x86_32 = lpad(hex(Murmur3.x86.hash32(test["text"], test["seed"])), 8, "0")
+        x86_128 = lpad(hex(Murmur3.x86.hash128(test["text"], test["seed"])), 32, "0")
+        x64_128 = lpad(hex(Murmur3.x64.hash128(test["text"], test["seed"])), 32, "0")
     else
         x86_32 = lpad(hex(Murmur3.x86.hash32(test["text"])), 8, "0")
         x86_128 = lpad(hex(Murmur3.x86.hash128(test["text"])), 32, "0")
         x64_128 = lpad(hex(Murmur3.x64.hash128(test["text"])), 32, "0")
     end
 
-    test["x86_32"] = x86_32
-    test["x86_128"] = x86_128
-    test["x64_128"] = x64_128
+    @test test["x86_32"] == x86_32
+    @test test["x86_128"] == x86_128
+    @test test["x64_128"] == x64_128
 
 end
-
-f = open("test/tests.json", "w")
-JSON.print(f, tests)
-close(f)
 
